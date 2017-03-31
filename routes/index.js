2a0a95e86e52420a2d20ef5ec154e105b61257ad
Patrick <ox6185ys@go.minneapolis.edu>
2017-03-30 19:55:55 -0500

    Mongoose First Run

--- routes/index.js
+++ routes/index.js
@@ -5,24 +5,26 @@ var Task = require('../models/task.js');
 
 /* GET home page, a list of incomplete tasks . */
 router.get('/', function(req, res, next) {
-// Task schema get matches.  tasks guarantees a return.
-  Task.find({completed:false}, function(err, tasks){
-    if (err) {
-      return next(err);
-    }
-    res.render('index', { title: 'TODO list' , tasks: tasks });
-  });
+
+    Task.find({completed:false}, function(err, tasks){
+        if (err) {
+            return next(err);
+        }
+        res.render('index', { title: 'TODO list' , tasks: tasks });
+    });
 });
+
+
 
 /* GET all completed tasks. */
 router.get('/completed', function(req, res, next){
 
-  Task.find({completed:true}, function(err, tasks){
-    if (err) {
-      return next(err);
-    }
-    res.render('tasks_completed', { title: 'TODO list' , tasks: tasks });
-  });
+    Task.find({completed:true}, function(err, tasks){
+        if (err) {
+            return next(err);
+        }
+        res.render('tasks_completed', { title: 'TODO list' , tasks: tasks });
+    });
 
 });
 
@@ -30,53 +32,52 @@ router.get('/completed', function(req, res, next){
 /* Mark all tasks as done. */
 router.post('/alldone', function(req, res, next){
 
-  Task.update( {completed:false}, {completed:true}, {multi:true}, function(err){
+    Task.update( {completed:false}, {completed:true}, {multi:true}, function(err){
 
-    if (err) {
-      return next(err);
-    }
+        if (err) {
+            return next(err);
+        }
 
-    req.flash('info', 'All tasks are done!');
-    return res.redirect('/')
+        req.flash('info', 'All tasks are done!');
+        return res.redirect('/')
 
-  });
+    });
 });
 
 
 
 
 /* Show details of one task */
-// Build the URL /task/:id
 router.get('/task/:id', function(req, res, next){
 
-  Task.findById(req.params.id, function(err, task){
-    if (err) {
-      return next(err);
-    }
-    return res.render('task_detail', {task:task})
-  })
+    Task.findById(req.params.id, function(err, task){
+        if (err) {
+            return next(err);
+        }
+        return res.render('task_detail', {task:task})
+    })
 });
 
 
 /* POST Add new task, then redirect to task list */
 router.post('/add', function(req, res, next){
 
-  if (!req.body || !req.body.text) {
-    req.flash('error', 'Please enter some text');
-    res.redirect('/');
-  }
+    if (!req.body || !req.body.text) {
+        req.flash('error', 'Please enter some text');
+        res.redirect('/');
+    }
 
-  else {
-    // Save new task with text provided, and completed = false
-    var task = Task({ text : req.body.text, completed: false});
+    else {
+        // Save new task with text provided, and completed = false
+        var task = Task({ text : req.body.text, completed: false});
 
-    task.save(function(err) {
-      if (err) {
-        return next(err);
-      }
-      return res.redirect('/')
-    });
-  }
+        task.save(function(err) {
+            if (err) {
+                return next(err);
+            }
+            return res.redirect('/')
+        });
+    }
 
 });
 
@@ -84,23 +85,23 @@ router.post('/add', function(req, res, next){
 /* Mark a task as done. Task _id should be provided as req.body parameter */
 router.post('/done', function(req, res, next){
 
-  var id = req.body._id;
-  Task.findByIdAndUpdate(id, {completed:true}, function(err, task){
+    var id = req.body._id;
+    Task.findByIdAndUpdate(id, {completed:true}, function(err, task){
 
-    if (err) {
-      return next(err);
-    }
+        if (err) {
+            return next(err);
+        }
 
-    if (!task) {
-      var req_err = new Error('Task not found');
-      req_err.status = 404;
-      return next(req_err);     // Task not found error
-    }
+        if (!task) {
+            var req_err = new Error('Task not found');
+            req_err.status = 404;
+            return next(req_err);     // Task not found error
+        }
 
-    req.flash('info', 'Task marked as done');
-    return res.redirect('/')
+        req.flash('info', 'Task marked as done');
+        return res.redirect('/')
 
-  });
+    });
 
 });
 
@@ -108,18 +109,18 @@ router.post('/done', function(req, res, next){
 /* Delete a task. Task _id is in req.body */
 router.post('/delete', function(req, res,next){
 
-  var id = req.body._id;
+    var id = req.body._id;
 
-  Task.findByIdAndRemove(id, function(err){
+    Task.findByIdAndRemove(id, function(err){
 
-    if (err) {
-      return next(err);    // For database errors
-    }
+        if (err) {
+            return next(err);    // For database errors
+        }
 
-    req.flash('info', 'Deleted');
-    return res.redirect('/')
+        req.flash('info', 'Deleted');
+        return res.redirect('/')
 
-  })
+    })
 });
 
 
